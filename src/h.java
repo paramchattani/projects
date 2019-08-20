1d5f8d2d915aa71d81489d988a6d7456612bcb48
paramchattani <37595290+paramchattani@users.noreply.github.com>
2019-08-20 15:47:07 +0530

    Add files via upload
    The main objective of the Online Movie Ticket Booking System is to manage the details of Movie,Ticket. It manages all the information about Movie, Booking.

---
+++ src/h.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ganesh
+ */
+class h {
+
+    static void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+}
