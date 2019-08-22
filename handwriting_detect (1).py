a7a8d779ef8cfc17c0389d0ae8f5f4a94715c6c9
paramchattani <37595290+paramchattani@users.noreply.github.com>
2019-08-22 11:10:37 +0530

    Update handwriting_detect (1).py

--- handwriting_detect (1).py
+++ handwriting_detect (1).py
@@ -1,6 +1,8 @@
 from sklearn import datasets
 import matplotlib.pyplot as plt
 from sklearn import svm
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
 digits=datasets.load_digits()
 #dir(digits)
 #print(digits.DESCR)
@@ -16,3 +18,9 @@ result=train.predict([digits.data[-5]])
 print(result)
 plt.imshow(digits.images[-5],cmap=plt.cm.gray_r)
 plt.show()
+x_train,x_test,y_train,y_test=train_test_split(digits.data,digits.target,test_size=0.3,random_state=1)
+clfs=svm.SVC(gamma=0.005,C=500)
+a=clfs.fit(x_train,y_train)
+y_pred=a.predict(x_test);
+print(metrics.confusion_matrix(y_test,y_pred))
+print(metrics.classification_report(y_test,y_pred))
