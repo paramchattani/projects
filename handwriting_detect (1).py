1a6bd1f27bba9b69caf23ce6a56ac913a30eff0c
paramchattani <37595290+paramchattani@users.noreply.github.com>
2019-08-22 11:12:37 +0530

    Update handwriting_detect (1).py

--- handwriting_detect (1).py
+++ handwriting_detect (1).py
@@ -22,5 +22,6 @@ x_train,x_test,y_train,y_test=train_test_split(digits.data,digits.target,test_si
 clfs=svm.SVC(gamma=0.005,C=500)
 a=clfs.fit(x_train,y_train)
 y_pred=a.predict(x_test);
+print(metrics.accuracy_score(y_test,y_pred))
 print(metrics.confusion_matrix(y_test,y_pred))
 print(metrics.classification_report(y_test,y_pred))
